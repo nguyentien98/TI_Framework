8f8e172ac2428de79d2eb375097cb69f043da2b0
nguyen.van.tien <FRAMGIA\nguyen.van.tien@framgia0171-pc.framgia.com>
2018-12-10 09:43:39 +0700

    Fix PHP convention

--- Core/DBConnection.php
+++ Core/DBConnection.php
@@ -18,8 +18,9 @@ class DBConnection
         try {
             $connection = new PDO(
                 'mysql:host=' . $config['host'] . ';dbname=' . $config['database'],
-                $config['username'], $config['password'],
-                array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')
+                $config['username'],
+                $config['password'],
+                [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8']
             );
 
             return $connection;
